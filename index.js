43ffad72f1ec3cf22ce7f26cd7d07d8b9021edd2
ImJaeOne <dlawi0108@naver.com>
2023-03-17 02:30:08 +0900

    innerHTML 이용해서 main에 들어갈 내용 바꾸기
    하는 중...

---
+++ index.js
@@ -0,0 +1,11 @@
+let title = document.getElementById("title")
+let content = document.getElementById("content")
+let uprightrow = document.getElementsByClassName("uprightrow")
+
+title.onclick = function(){
+    content.innerHTML = 
+}
+
+uprightrow[0].onclcik = function(){
+    content.innerHTML = 
+}
