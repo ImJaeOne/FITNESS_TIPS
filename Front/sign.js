b68b419bdf1c37cbd1da88fcb8e630d6a71c8650
ImJaeOne <dlawi0108@naver.com>
2023-05-10 16:30:27 +0900

    회원가입 유효성 검사 만드는중

--- Front/sign.js
+++ Front/sign.js
@@ -2,12 +2,15 @@ const joinId = document.getElementsByClassName("join_id")[0];
 const Circle = document.getElementsByClassName("circle")[0];
 const Id = document.getElementsByClassName("member_field")[0];
 
-function ID(){
-    if(joinId.value == ""){
-        Circle.classList.add('incorrect');
-    }
-    else{
+joinId.addEventListener('focus',function ID(){
+    if(joinId.value != "" ){
+        Circle.classList.remove('incorrect');
         Circle.classList.add('correct');
-        console.log(e);
+        console.log("a");
     }
-}
+    else if(joinId.value == ""){
+        Circle.classList.remove('correct');
+        Circle.classList.add('incorrect');
+        console.log("e");
+    }
+})
