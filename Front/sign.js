ad1bc4d5bd0b7d98b3ed4df7dacf4aa084299398
ImJaeOne <dlawi0108@naver.com>
2023-05-15 03:01:54 +0900

    회원가입 유효성 검사 오류 잡음

--- Front/sign.js
+++ Front/sign.js
@@ -1,11 +1,12 @@
+
 //아이디 유효성 검사
 
 const joinId = document.getElementsByClassName("join_id")[0];
 const Circle_id = document.getElementsByClassName("circle")[0];
+const reg_ID = /^([a-z0-9]{6,12})+$/;
 
 joinId.addEventListener('input', function(){
-    const reg_ID = /^[a-z0-9]+$/;
-    if(!reg_ID.test(joinId.value) || joinId.value.length < 6 || joinId.value.length > 12){
+    if(!reg_ID.test(joinId.value)){
         Circle_id.classList.add('incorrect');
         Circle_id.classList.remove('correct');
     }
@@ -17,26 +18,24 @@ joinId.addEventListener('input', function(){
 
 //비밀먼호 유효성 검사
 
-const joinPw = document.getElementsByClassName("join_pw")[0]
+const joinPw = document.getElementsByClassName("join_pw")[0];
 const Circle_pw = document.getElementsByClassName("circle")[1];
+const reg_PW = /^([A-Za-z0-9]{6,12})+$/;
 
 joinPw.addEventListener('input', function(){
-    const reg_PW = /^[A-za-z0-9]+$/;
-    if(!reg_PW.test(joinPw.value) || joinPw.value.length < 6 || joinPw.value.length > 12){
+    if(!reg_PW.test(joinPw.value)){
         Circle_pw.classList.add('incorrect');
         Circle_pw.classList.remove('correct');
-        console.log("N");
     }
     else{
         Circle_pw.classList.add('correct');
         Circle_pw.classList.remove('incorrect');
-        console.log("E");
     }
 })
 
 //비밀번호 확인
 
-const joinPwcf= document.getElementsByClassName("join_pwcf")[0]
+const joinPwcf= document.getElementsByClassName("join_pwcf")[0];
 const Circle_pwcf = document.getElementsByClassName("circle")[2];
 
 joinPwcf.addEventListener('input', function(){
@@ -52,21 +51,38 @@ joinPwcf.addEventListener('input', function(){
 
 //이름 유효성 검사
 
-const joinName = document.getElementsByClassName("join_name")[0]
+const joinName = document.getElementsByClassName("join_name")[0];
 const Circle_name = document.getElementsByClassName("circle")[3];
+const reg_NAME = /^[가-힣]{2,5}$/;
 
 joinName.addEventListener('input', function(){
-    const reg_NAME = /^[가-힣]{2,5}$/;
     if(!reg_NAME.test(joinName.value)){
         Circle_name.classList.add('incorrect');
         Circle_name.classList.remove('correct');
-        console.log("N");
     }
     else{
         Circle_name.classList.add('correct');
         Circle_name.classList.remove('incorrect');
-        console.log("E");
     }
 })
 
+const Confirm = document.getElementById("confirm");
 
+Confirm.addEventListener("mouseup",function(){
+    if(!reg_ID.test(joinId.value)){
+        joinId.focus();
+    }
+    else if(!reg_PW.test(joinPw.value)){
+        joinPw.focus();
+    }
+    else if(joinPwcf.value != joinPw.value){
+        joinPwcf.focus();
+    }
+    else if(!reg_NAME.test(joinName.value)){
+        joinName.focus();
+    }
+    else {
+        alert('회원가입에 성공하셨습니다.');
+        history.back();
+    }
+})
